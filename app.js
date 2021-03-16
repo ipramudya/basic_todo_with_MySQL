45e2ec939d51d05324fe989d25fdadb6b3b57e35
ipramudya <ipramudya.dev@gmail.com>
2021-03-16 22:22:46 +0700

    Intro Basic Todo List with MySQL

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded());
 // Router
 app.use("/todos", todosRouter);
 
-// app.use("/", todosError);
+app.use("/", todosError);
 
 const port = 5000;
 app.listen(port, () => {
